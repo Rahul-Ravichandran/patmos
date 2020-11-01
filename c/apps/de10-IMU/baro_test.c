13e9b84e2bfbe56ec75d407b0aca1958c35d6fa2
Rahul <rravic19@student.aau.dk>
2020-11-01 15:04:06 +0100

    baro minor cange

--- c/apps/de10-IMU/baro_test.c
+++ c/apps/de10-IMU/baro_test.c
@@ -95,16 +95,21 @@ bool first_angle=false;
 float angle_roll_acc=0.0, angle_pitch_acc=0.0, angle_pitch=0.0, angle_roll=0.0;
 
 //Barometer variables.
-unsigned int C[7];
-unsigned int barometer_counter, temperature_counter;
-int OFF, OFF_C2, SENS, SENS_C1, P;
-unsigned int raw_pressure, raw_temperature, temp;
-float actual_pressure, actual_pressure_slow, actual_pressure_fast, actual_pressure_diff;
-float ground_pressure, altutude_hold_pressure;
-int dT, dT_C5;
-unsigned int start;
+// unsigned int C[7];
+// unsigned int barometer_counter, temperature_counter;
+// int OFF, OFF_C2, SENS, SENS_C1, P;
+// unsigned int raw_pressure, raw_temperature, temp;
+// float actual_pressure, actual_pressure_slow, actual_pressure_fast, actual_pressure_diff;
+// float ground_pressure, altutude_hold_pressure;
+// int dT, dT_C5;
+// unsigned int start;
 
+//Barometer v2 variables
+#define MS5611_ADDR 0x77
 
+unsigned long Coff[6], Ti = 0, offi = 0, sensi = 0;
+unsigned int data[3];
+//////////////
 
 unsigned int signature = 0;
 unsigned int ACCEL_X_H = 0;
@@ -125,6 +130,17 @@ unsigned int GYRO_Z_L = 0;
 //Writes to i2c, returns -1 if there was an error, 0 if succeded
 int i2c_write(unsigned char chipaddress, unsigned char regaddress, unsigned char data){
   I2C = ((((unsigned int) data & 0x000000FF) << 16) | (((unsigned int) regaddress & 0x000000FF) << 8) | (((unsigned int) chipaddress & 0x0000007F) << 1)) & 0xFFFFFFFE;
+  if ((I2C & 0x00000100) != 0)
+  {
+    return -1;
+  }else{
+    return 0;
+  }
+}
+
+//Writes to i2c, returns -1 if there was an error, 0 if succeded
+int i2c_write_short(unsigned char chipaddress, unsigned char regaddress){
+  I2C = ((((unsigned int) regaddress & 0x000000FF) << 8) | (((unsigned int) chipaddress & 0x0000007F) << 1)) & 0xFFFFFFFE;
   if ((I2C & 0x00000100) != 0)
   {
     return -1;
@@ -162,103 +178,186 @@ void LED_out(int i){
   else LED = 0x0000;
   return;
 }
-void check_barometer(void) {
-  loop_counter = 0;
 
-    //For calculating the pressure the 6 calibration values need to be polled from the MS5611.
-    //These 2 byte values are stored in the memory location 0xA2 and up.
-    for (start = 1; start <= 6; start++) {
-      C[start] = i2c_read(MS5611_address, 0xA0 + start*2) << 8 | i2c_read(MS5611_address, 0xA0 + start*2 +1);                    //Start communication with the MPU-6050.
-    }
-    //Print the 6 calibration values on the screen.
-    printf("C1 = %d\n",C[1]);
-    printf("C2 = %d\n",C[2]);
-    printf("C3 = %d\n",C[3]);
-    printf("C4 = %d\n",C[4]);
-    printf("C5 = %d\n",C[5]);
-    printf("C6 = %d\n",C[6]);
+void check_barometerv2(void)
+{
+  // printf("random = %X",i2c_read(MS5611_ADDR, 0x48));
+  i2c_write_short(MS5611_ADDR, 0x1E);
+  millis(5);
+  i2c_write_short(MS5611_ADDR, 0x48);
+  millis(10);
+  
+  data[0] = i2c_read(MS5611_ADDR, 0x00);
+  data[1] = i2c_read(MS5611_ADDR, 0x00+1);
+  data[2] = i2c_read(MS5611_ADDR, 0x00+2);
+  
+  unsigned long ptemp = ((data[0] * 65536.0) + (data[1] * 256.0) + data[2]);
+  
+  // printf("data[0] = %X  ",data[0]);
+  // printf("data[1] = %X  ",data[1]);
+  // printf("data[2] = %X",data[2]);
 
-    OFF_C2 = C[2] * pow(2, 16);                                   //This value is pre-calculated to offload the main program loop.
-    SENS_C1 = C[1] * pow(2, 15);                                  //This value is pre-calculated to offload the main program loop.
+  i2c_write_short(MS5611_ADDR, 0x58);
+  millis(10);
+  
+  data[0] = i2c_read(MS5611_ADDR, 0x00);
+  data[1] = i2c_read(MS5611_ADDR, 0x00+1);
+  data[2] = i2c_read(MS5611_ADDR, 0x00+2);
 
-    start = 0;
+  // Convert the data
+  unsigned long temp = ((data[0] * 65536.0) + (data[1] * 256.0) + data[2]);
+  
+  unsigned long dT = temp - ((Coff[4] * 256));
+  temp = 2000 + (dT * (Coff[5] / pow(2, 23)));
+  
+  unsigned long long off = Coff[1] * 65536 + (Coff[3] * dT) / 128;
+  unsigned long long sens = Coff[0] * 32768 + (Coff[2] * dT) / 256;
 
-  for(int i=0;i<1000;i++) {                                           //Stay in this loop until the data variable data holds a q.
-    loop_timer = get_cpu_usecs() + 4000;                                 //Set the loop_timer variable to the current micros() value + 4000.
-
-    barometer_counter ++;                                         //Increment the barometer_counter variable for the next step.
-
-    if (barometer_counter == 1) {
-      if (temperature_counter == 0) {
-        //Get temperature data from MS-5611
-        raw_temperature = i2c_read(MS5611_address, 0x58) << 16 | i2c_read(MS5611_address, 0x58 + 1) << 8 | i2c_read(MS5611_address, 0x58 + 2);
-      }
-      else {
-        //Get pressure data from MS-5611
-        raw_pressure = i2c_read(MS5611_address, 0x48) << 16 | i2c_read(MS5611_address, 0x48 + 1) << 8 | i2c_read(MS5611_address, 0x48 + 2);
-      }
-
-      temperature_counter ++;
-      if (temperature_counter > 9) {
-        temperature_counter = 0;
-        //Request temperature data
-        i2c_write(MS5611_address, 0x58, 0x00);
-      }
-      else {
-        //Request pressure data
-        i2c_write(MS5611_address, 0x48, 0x00);
-      }
-    }
-    if (barometer_counter == 2) {
-      //Calculate pressure as explained in the datasheet of the MS-5611.
-      dT = C[5];
-      dT <<= 8;
-      dT *= -1;
-      dT += raw_temperature;
-
-      OFF = OFF_C2 + ((int)dT * (int)C[4]) / pow(2, 7);
-
-      SENS = SENS_C1 + ((int)dT * (int)C[3]) / pow(2, 8);
-
-      P = ((raw_pressure * SENS) / pow(2, 21) - OFF) / pow(2, 15);
-
-      if (actual_pressure == 0) {
-        actual_pressure = P;
-        actual_pressure_fast = P;
-        actual_pressure_slow = P;
-      }
-
-      actual_pressure_fast = actual_pressure_fast * (float)0.92 + P * (float)0.08;
-      actual_pressure_slow = actual_pressure_slow * (float)0.99 + P * (float)0.01;
-      actual_pressure_diff = actual_pressure_slow - actual_pressure_fast;
-      if (actual_pressure_diff > 8)actual_pressure_diff = 8;
-      if (actual_pressure_diff < -8)actual_pressure_diff = -8;
-      if (actual_pressure_diff > 1 || actual_pressure_diff < -1)actual_pressure_slow -= actual_pressure_diff / 6.0;
-      actual_pressure = actual_pressure_slow;
-      if (start < 200){
-        start++;
-        actual_pressure = 0;
-      }
-      else printf("%f \n", actual_pressure);
-    }
-    if (barometer_counter == 3) {
-      barometer_counter = 0;
-    }
-    while (loop_timer > get_cpu_usecs());
+  if(temp >= 2000)
+  {
+     Ti = 0;
+     offi = 0;
+     sensi = 0;
   }
-  loop_counter = 0;                                                                     //Reset the loop counter variable to 0.
-  start = 0;
+  else if(temp < 2000)
+  {
+    Ti = (dT * dT) / (pow(2,31));
+    offi = 5 * ((pow((temp - 2000), 2))) / 2;
+    sensi =  5 * ((pow((temp - 2000), 2))) / 4; 
+    if(temp < -1500)
+    {
+       offi = offi + 7 * ((pow((temp + 1500), 2)));      
+       sensi = sensi + 11 * ((pow((temp + 1500), 2))) / 2;
+    }
+  }
+   
+  temp -= Ti;
+  off -= offi;
+  sens -= sensi;
+
+  ptemp = (ptemp * sens / 2097152 - off);
+  ptemp /= 32768;
+  float pressure = ptemp / 100.0;
+  float ctemp = temp / 100.0;
+  float fTemp = ctemp * 1.8 + 32.0;
+
+  // Output data to serial monitor
+  printf("Temperature in Celsius : %f            ",ctemp);
+  printf(" Pressure : %f ",pressure);
+  printf(" mbar \n"); 
+  millis(10);
+
 }
+// void check_barometer(void) {
+//   loop_counter = 0;
+
+//     //For calculating the pressure the 6 calibration values need to be polled from the MS5611.
+//     //These 2 byte values are stored in the memory location 0xA2 and up.
+//     for (start = 1; start <= 6; start++) {
+//       C[start] = i2c_read(MS5611_address, 0xA0 + start*2) << 8 | i2c_read(MS5611_address, 0xA0 + start*2 +1);                    //Start communication with the MPU-6050.
+//     }
+//     //Print the 6 calibration values on the screen.
+//     printf("C1 = %d\n",C[1]);
+//     printf("C2 = %d\n",C[2]);
+//     printf("C3 = %d\n",C[3]);
+//     printf("C4 = %d\n",C[4]);
+//     printf("C5 = %d\n",C[5]);
+//     printf("C6 = %d\n",C[6]);
+
+//     OFF_C2 = C[2] * pow(2, 16);                                   //This value is pre-calculated to offload the main program loop.
+//     SENS_C1 = C[1] * pow(2, 15);                                  //This value is pre-calculated to offload the main program loop.
+
+//     start = 0;
+
+//   for(int i=0;i<100000;i++) {                                           //Stay in this loop until the data variable data holds a q.
+//     loop_timer = get_cpu_usecs() + 4000;                                 //Set the loop_timer variable to the current micros() value + 4000.
+
+//     barometer_counter ++;                                         //Increment the barometer_counter variable for the next step.
+
+//     if (barometer_counter == 1) {
+//       if (temperature_counter == 0) {
+//         //Get temperature data from MS-5611
+//         raw_temperature = i2c_read(MS5611_address, 0x00) << 16 | i2c_read(MS5611_address, 0x00 + 1) << 8 | i2c_read(MS5611_address, 0x00 + 2);
+//       }
+//       else {
+//         //Get pressure data from MS-5611
+//         raw_pressure = i2c_read(MS5611_address, 0x00) << 16 | i2c_read(MS5611_address, 0x00 + 1) << 8 | i2c_read(MS5611_address, 0x00 + 2);
+//       }
+
+//       temperature_counter ++;
+//       if (temperature_counter > 9) {
+//         temperature_counter = 0;
+//         //Request temperature data
+//         i2c_write(MS5611_address, 0x58, 0x00);
+//       }
+//       else {
+//         //Request pressure data
+//         i2c_write(MS5611_address, 0x48, 0x00);
+//       }
+//     }
+//     if (barometer_counter == 2) {
+//       //Calculate pressure as explained in the datasheet of the MS-5611.
+//       dT = C[5];
+//       dT <<= 8;
+//       dT *= -1;
+//       dT += raw_temperature;
+
+//       OFF = OFF_C2 + ((int)dT * (int)C[4]) / pow(2, 7);
+
+//       SENS = SENS_C1 + ((int)dT * (int)C[3]) / pow(2, 8);
+
+//       P = ((raw_pressure * SENS) / pow(2, 21) - OFF) / pow(2, 15);
+
+//       if (actual_pressure == 0) {
+//         actual_pressure = P;
+//         actual_pressure_fast = P;
+//         actual_pressure_slow = P;
+//       }
+
+//       actual_pressure_fast = actual_pressure_fast * (float)0.92 + P * (float)0.08;
+//       actual_pressure_slow = actual_pressure_slow * (float)0.99 + P * (float)0.01;
+//       actual_pressure_diff = actual_pressure_slow - actual_pressure_fast;
+//       if (actual_pressure_diff > 8)actual_pressure_diff = 8;
+//       if (actual_pressure_diff < -8)actual_pressure_diff = -8;
+//       if (actual_pressure_diff > 1 || actual_pressure_diff < -1)actual_pressure_slow -= actual_pressure_diff / 6.0;
+//       actual_pressure = actual_pressure_slow;
+//       if (start < 200){
+//         start++;
+//         actual_pressure = 0;
+//       }
+//       else printf("%f \n", actual_pressure);
+//     }
+//     if (barometer_counter == 3) {
+//       barometer_counter = 0;
+//     }
+//     while (loop_timer > get_cpu_usecs());
+//   }
+//   loop_counter = 0;                                                                     //Reset the loop counter variable to 0.
+//   start = 0;
+// }
 
 int main(int argc, char **argv)
 {
   printf("Hello Baro!\n");
   
+  for (int i = 0; i < 6; i++) {
+      data[0] = i2c_read(MS5611_address, 0xA2 + i*2);
+      data[1] = i2c_read(MS5611_address, 0xA2 + i*2 +1);                    //Start communication with the MPU-6050.
+      Coff[i] = ((data[0]*256) + data[1]);
 
+      printf("data[0] = %X  ",data[0]);
+      printf("data[1] = %X  \n",data[1]);
+    }
+    printf("C1 = %lu\n",Coff[0]);
+    printf("C2 = %lu\n",Coff[1]);
+    printf("C3 = %lu\n",Coff[2]);
+    printf("C4 = %lu\n",Coff[3]);
+    printf("C5 = %lu\n",Coff[4]);
+    printf("C6 = %lu\n",Coff[5]);
   // loop_timer = get_cpu_usecs();
   
-  // for(int i=0;i<100;i++)
-  check_barometer();
+  for(int i=0;i<1000;i++)
+  check_barometerv2();
   return 0;
 }
   
